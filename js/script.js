a0fe7a8e0104da89acc39ab5ad294a2a12f3c5d2
amarasalah <amarasalah@outlook.com>
2022-08-27 11:52:12 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const btnEl= document.getElementById("btn");
 btnEl.addEventListener("mouseover", (e)=>{
     y = e.pageY - btnEl.offsetTop ;
     x = e.pageX - btnEl.offsetLeft;
-    console.log(btnEl.style.setProperty("--xIndex", x + "px" ));
+    btnEl.style.setProperty("--xIndex", x + "px" );
     btnEl.style.setProperty("--yIndex", y + "px" );
 
 })
